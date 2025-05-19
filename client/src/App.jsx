f5c0ec5bee76078805a13ee62c91e4090be9cef4
Jil Rupani <jilrupani04@gmail.com>
2025-05-19 13:08:51 +0000

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useEffect} from 'react';
+import {Container, AppBar ,Typography,Grow,Grid} from '@mui/material'
+import {useDispatch} from 'react-redux';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {getPosts} from './actions/posts.jsx'
+import Posts from './components/Posts/Posts.jsx';
+import Form from './components/Form/Form.jsx';
+import memories from '../src/images/memories.png';
+import useStyles from './styles.js';
+
+ const App = () => {
+  const classes= useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(getPosts());
+  },[dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxwidth="lg">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center" >Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>  
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/reducers/posts.jsx
@@ -0,0 +1,12 @@
+import Posts from "../components/Posts/Posts";
+
+const reducer = (state,action) =>{
+    switch(action.type){
+        case 'FETCH_ALL':
+            return state;
+        case 'CREATE':
+            return Posts;
+        default:
+            return Posts;
+    }
+}
