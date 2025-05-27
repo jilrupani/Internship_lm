03fcd78a2bc29795c9f8474aee20ed3aee79102e
Jil Rupani <jilrupani04@gmail.com>
2025-05-27 13:09:59 +0000

    commit

--- memories/client/src/components/Posts/Post/Post.jsx
+++ memories/client/src/components/Posts/Post/Post.jsx
@@ -16,7 +16,7 @@ function Post({post, setCurrentId}) {
 
   const Likes = () => {
     if (post.likes.length > 0) {
-      return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+      return post.likes.find((like) => like === (user?.result?.sub || user?.result?._id))
         ? (
           <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
         ) : (
@@ -35,7 +35,7 @@ function Post({post, setCurrentId}) {
       <Typography variant="h6">{post.name}</Typography>
       <Typography variant="body">{moment(post.createdAt).fromNow()}</Typography>
     </div>
-    {(user?.result?.googleId ===  post?.creator || user?.result?._id === post?.creator) && (
+    {(user?.result?.sub ===  post?.creator || user?.result?._id === post?.creator) && (
     <div className={classes.overlay2}>
       <Button 
         style={{color: 'white'}} 
@@ -56,7 +56,7 @@ function Post({post, setCurrentId}) {
       <Button size="small" color='primary' disabled={!user?.result} onClick={()=> dispatch(likePost(post._id))}>
         <Likes/>
       </Button>
-      {(user?.result?.googleId ===  post?.creator || user?.result?._id === post?.creator) && (
+      {(user?.result?.sub ===  post?.creator || user?.result?._id === post?.creator) && (
       <Button size="small" color='primary' onClick={()=> dispatch(deletePost(post._id))}>
         <DeleteIcon fontSize="small"/>
         DELETE
